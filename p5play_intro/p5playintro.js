c8c20c869bc291690ae3bf0021e61357855292a7
ThemeGithub <olliken17@gmail.com>
2025-07-16 09:01:23 +0000

    commit

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,3 +1,6 @@
+let ball;
+let box;
+
 function setup() {
   // Set up the canvas
   new Canvas(800, 400);
@@ -5,14 +8,35 @@ function setup() {
 
   // Basic shape testing
   // write your codes here
- 
+  ball = new Sprite;
+  ball.x = 100
+  ball.y = 200
+  ball.diameter = 40;
+  ball.color = "lime";
+  ball.stroke = "orange";
+  ball.strokeWeight = 10;
+
+  box = new Sprite;
+  box.x = 100;
+  box.y = 100;
+  box.w = 80;
+  box.h = 40;
+  box.color = "yellow"
+  box.stroke = "cyan"
+  box.strokeWeight = 10;
   // End Basic shape testing
 
    // Create a bouncing ball sprite
    // write your codes here
-
+  draw()
 }
 
 function draw() {
-  // write your codes here
+  // fill("green");
+  // stroke("orange");
+  // strokeWeight(10);
+
+  // circle(30,30,50);
+  // rect(50,50,100,200);
+  // rect(150,250,50,50);
 }
