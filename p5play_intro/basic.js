807be833cbd8be49e4d9b7865c08dd1c151549ac
ThemeGithub <olliken17@gmail.com>
2025-07-30 08:04:28 +0000

    Jul 30, 2025, 8:04 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -24,11 +24,13 @@ function setup() {
   // }
 
 
-  
+
   let sum = 0
   for (let i=2;i<=20;i++) {
-    sum = sum + i
+    sum = sum + i;
   }
+  console.log(sum);
+
 
   // write your codes here
 
