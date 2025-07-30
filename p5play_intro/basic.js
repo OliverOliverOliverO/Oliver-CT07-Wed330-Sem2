bf098a8867186af370eab8c72516e023ddea4304
ThemeGithub <olliken17@gmail.com>
2025-07-30 07:46:05 +0000

    Jul 30, 2025, 7:46 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,16 +1,21 @@
 
 
 function setup() {
-  const base = 5;
-  const height = 10;
+  // const base = 5;
+  // const height = 10;
 
-  console.log(base * height / 2);
+  // console.log(base * height / 2);
 
-  new Canvas(800, 400);
-  background(250);
-  textSize(20);
-  text("the area is " + (base * height / 2), 10, 30);
+  // new Canvas(800, 400);
+  // background(250);
+  // textSize(20);
+  // text("the area is " + (base * height / 2), 10, 30);
 
+
+
+
+
+  
   // write your codes here
 
   // --- Exercise: Area of Triangle ---
