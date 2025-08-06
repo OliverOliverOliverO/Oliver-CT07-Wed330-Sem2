ad2b74ae78f4681a625e4b2f6f9684e63f8123ad
ThemeGithub <olliken17@gmail.com>
2025-08-06 08:07:22 +0000

    Aug 6, 2025, 8:07 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -63,7 +63,7 @@ function setup() {
   list.unshift("g");
   list.splice(4, 0, "h");
   list.splice(1,1);
-  list.splice(namesindexOf(""))
+  list.splice(namesindexOf("f"),1);
 
 
   for(let i=0;i<list.length;i++) {
