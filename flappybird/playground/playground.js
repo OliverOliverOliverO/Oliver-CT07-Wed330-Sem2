1c7daf12b43e62725dc21cf72efda06022973476
ThemeGithub <olliken17@gmail.com>
2025-08-16 08:09:49 +0000

    Aug 16, 2025, 8:09 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -47,10 +47,10 @@ function draw(){
     bird.sleeping = false;
   }
 
-  if(mouse.presses('left')){
-    bird2 = new Sprite(mouse.x,mouse.y,30,30,'dynamic');
-    bird2.img = flapMidImg
-  }
+//   if(mouse.presses('left')){
+//     bird2 = new Sprite(mouse.x,mouse.y,30,30,'dynamic');
+//     bird2.img = flapMidImg
+//   }
   if(mouse.presses('right')){
     bird2= new Sprite(mouse.x,mouse.y,30,30,'static');
     bird2.img = flapMidImg
