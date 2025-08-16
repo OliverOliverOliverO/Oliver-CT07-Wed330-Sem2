32f8f2b006a46bfc76cdb244d35a8c17a49d8dd2
ThemeGithub <olliken17@gmail.com>
2025-08-16 09:11:37 +0000

    Aug 16, 2025, 9:11 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -72,7 +72,7 @@ function draw(){
 }
 function spawnPipePair() {
   let gap = 50;
-  let midY = 1;
+  let midY = 200;
 
   topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
   topPipe.img = pipe;
