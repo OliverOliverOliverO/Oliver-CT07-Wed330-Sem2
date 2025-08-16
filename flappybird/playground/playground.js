c9bc85f1310ad3a4a94ad76a498d8d83af0b97a2
ThemeGithub <olliken17@gmail.com>
2025-08-16 09:16:24 +0000

    Aug 16, 2025, 9:16 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -75,11 +75,11 @@ function spawnPipePair() {
   let gap = 50;
   let midY = height / 2;
 
-  topPipe = new Sprite(400, midY - gap / 2 -200, 52, 200, 'static');
+  topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
   topPipe.img = pipeImg;
   topPipe.rotation = 180;
   
-  bottomPipe = new Sprite(400, midY + gap / 2 +200, 52, 320, 'static');
+  bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
   bottomPipe.img = pipeImg;
 
   pipeGroup.add(topPipe);
