219d920a5a69520771d6001b6b9d30764a276a41
ThemeGithub <olliken17@gmail.com>
2025-09-03 07:54:32 +0000

    Sep 3, 2025, 7:54 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -1,1 +1,93 @@
 // the final game
+
+let bird , floor;
+let bg, base, flapMidImg;
+let bird2;
+let pipeGroup;
+let pipe;
+let topPipe, bottomPipe;
+//load the images before game starts
+function preload(){
+  bg = loadImage('assets/background-day.png');
+  base = loadImage('assets/base.png');
+
+  flapMidImg = loadImage('assets/yellowbird-midflap.png');
+  flapUpImg = loadImage('assets/yellowbird-upflap.png');
+  flapDownImg = loadImage('assets/yellowbird-downflap.png');
+
+  pipeImg = loadImage('assets/pipe-green.png');
+}
+
+function setup(){
+//to create a new Canvas
+ new Canvas(400,600);
+ bird = new Sprite();
+ bird.x =  width/2;
+ bird.y = 200;
+ bird.width = 30;
+ bird.height = 30;
+ bird.img = flapMidImg;
+
+ //bird physics
+ bird.collider ='dynamic';
+ bird.mass =2;
+ bird.drag = 0.02;
+ bird.bounciness = 0.5;
+ world.gravity.y = 10;
+ 
+ floor = new Sprite();
+ floor.x = 200;
+ floor.y = height -20;
+ floor.width = 400;
+ floor.height = 125;
+ floor.collider = 'static';
+ floor.img = base;
+
+ pipeGroup = new Group();
+
+}
+
+function draw(){
+  image(bg,0,0,width,height); // draw the bg
+
+  if(kb.presses('space') || mouse.presses('left')){
+    bird.vel.y = -5;
+    bird.sleeping = false;
+  }
+
+
+  if(bird.vel.y < -1) {
+    bird.img = flapUpImg;
+    bird.rotation = -30;
+  } else if (bird.vel.y > 1) {
+    bird.img = flapDownImg;
+    bird.rotation = 30;
+  } else {
+    bird.img = flapMidImg;
+    bird.rotation = 0;
+  }
+
+  if(frameCount % 120 === 60) {
+    spawnPipePair();
+  }
+
+  pipeGroup.vel.x = -3;
+
+}
+
+function spawnPipePair() {
+  let gap = 50;
+  let midY = random(50, 350);
+
+  topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'kinematic');
+  topPipe.img = pipeImg;
+  topPipe.rotation = 180;
+  
+  bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'kinematic');
+  bottomPipe.img = pipeImg;
+
+  pipeGroup.add(topPipe);
+  pipeGroup.add(bottomPipe);
+
+  pipeGroup.layer = 0;
+}
