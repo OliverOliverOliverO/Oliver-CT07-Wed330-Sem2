dde423fca26170e60c49c8c2a1a64ff258fb1247
ThemeGithub <olliken17@gmail.com>
2025-09-10 09:00:10 +0000

    Sep 10, 2025, 9:00 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -60,7 +60,8 @@ function draw(){
   image(bg,0,0,width,height); // draw the bg
 
 
-  
+  image(bg,0,0,width,height);
+  if (kb.pr)
 
   if(kb.presses('space') || mouse.presses('left')){
     bird.vel.y = -5;
