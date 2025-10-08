77e31d5f81442afe41224cf13206a03d6ca8bebe
ThemeGithub <olliken17@gmail.com>
2025-10-08 08:31:03 +0000

    Oct 8, 2025, 8:31 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -83,7 +83,8 @@ function splitFruit(x, y, fruitData) {
     left.img = fruitData.half;
     left.vel.x = -3;
     left.vel.y = random(-5,-2);
-    left
+    left.rotationSpeed = -5;
+    left.life = 40;
 
     let right = new fruitHalves.Sprite(x+20, y, 40, 40)
     right.img = fruitData.half;
