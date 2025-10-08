7ff9b6597020130dada57bdd6f0106a22106f59d
ThemeGithub <olliken17@gmail.com>
2025-10-08 07:56:34 +0000

    Oct 8, 2025, 7:56 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -62,7 +62,7 @@ function splitFruit() {
         if(d < fruit.d/2+5) {
             fruit.sliced = true;
 
-            fruit.removed();
+            fruit.remove();
 
             break;
         }
