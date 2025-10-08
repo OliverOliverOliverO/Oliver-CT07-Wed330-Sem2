1eddfa61118f99e36136e5cb343f1aa01bb49cd6
ThemeGithub <olliken17@gmail.com>
2025-10-08 08:56:43 +0000

    Oct 8, 2025, 8:56 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -8,12 +8,12 @@ function setup() {
     bird = new Sprite(200, 300, 50);
     bird.img = birdImg;
 
-    bg = new Sprite(0, 0, 400, 600);
+    bg = new Sprite(0, 0, 800, 1200);
     bg.img = backgroundImg;
 
 
 }
 
 function draw() {
-    
+
 }
