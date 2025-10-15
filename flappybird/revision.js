45414f12d8b704182e6c01dfe984b2719dea8b78
ThemeGithub <olliken17@gmail.com>
2025-10-15 08:07:15 +0000

    Oct 15, 2025, 8:07 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -30,13 +30,13 @@
 
 function setup() {
     new Canvas(400, 600);
-    world.gravity.y = 10;
+    // world.gravity.y = 10;
     background(220);
     ball = new Sprite(200, 300, 30);
 }
 
 function draw() {
-    ball
+    ball.velocity.y = -1
 
 
 }
