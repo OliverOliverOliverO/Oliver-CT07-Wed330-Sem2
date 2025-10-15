3ada8798f1dfa3bc44e0b59576fc1ef1b4ef9abc
ThemeGithub <olliken17@gmail.com>
2025-10-15 08:40:42 +0000

    Oct 15, 2025, 8:40 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -49,7 +49,7 @@ function draw() {
     textSize(24);
     textAlign(LEFT, TOP);
     text('Score: ' + score, 10, 10)
-    text('MissedFruits: ' + missedFruit, 50, 10)
+    text('MissedFruits: ' + missedFruit, 200, 10)
     for(fruit of fruitGroup) {
         if(fruit.y > height+50) {
         missedFruit += 1
