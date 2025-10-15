a77e6650d18a7638a131faef529a0cd390a82acf
ThemeGithub <olliken17@gmail.com>
2025-10-15 08:35:25 +0000

    Oct 15, 2025, 8:35 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -66,7 +66,7 @@ function spawnFruit() {
 
 function sliceFruit() {
     for(let fruit of fruitGroup) {
-        if(fruit.y > 350 && fruit.vel.y > 0) {
+        if(fruit.y > 350 && fruit.vel.y < 0) {
             missedFruit += 1
             fruit.remove();
         }
