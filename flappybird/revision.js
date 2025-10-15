045df2e554ad736f640a7e459973ab958b795f3c
ThemeGithub <olliken17@gmail.com>
2025-10-15 08:04:37 +0000

    Oct 15, 2025, 8:04 AM

--- flappybird/revision.js
+++ flappybird/revision.js
@@ -1,28 +1,31 @@
-let bgImg;
-let birdImg;
-let bird;
-let bg;
+// let bgImg;
+// let birdImg;
+// let bird;
+// let bg;
 
-function preload() {
-    bgImg = loadImage('assets/background-day.png');
-    birdImg = loadImage('assets/yellowbird-midflap.png');
-}
+// function preload() {
+//     bgImg = loadImage('assets/background-day.png');
+//     birdImg = loadImage('assets/yellowbird-midflap.png');
+// }
 
-function setup() {
-    new Canvas(400, 600);
-    world.gravity.y = 10;
-    bird = new Sprite(200, 300, 30, 30, 'dynamic');
-    bird.img = birdImg;
-    bird.layer = 1;
+// function setup() {
+//     new Canvas(400, 600);
+//     world.gravity.y = 10;
+//     bird = new Sprite(200, 300, 30, 30, 'dynamic');
+//     bird.img = birdImg;
+//     bird.layer = 1;
 
-    bg = new Sprite(200, 300, width, height, 'none');
-    bg.img = bgImg
-    bg.layer = 0;
+//     bg = new Sprite(200, 300, width, height, 'none');
+//     bg.img = bgImg
+//     bg.layer = 0;
 
-}
+// }
 
-function draw() {
-    if(mouse.presses('left')) {
-        bird.vel.y = -5;
-    }
-}
+// function draw() {
+//     if(mouse.presses('left')) {
+//         bird.vel.y = -5;
+//     }
+// }
+
+
+function setup() P
