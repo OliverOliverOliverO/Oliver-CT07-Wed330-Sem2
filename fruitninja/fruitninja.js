39f1de38bbce1544aa5505c2cd93bb1b3b7c23f6
ThemeGithub <olliken17@gmail.com>
2025-10-29 08:02:22 +0000

    Oct 29, 2025, 8:02 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -50,7 +50,7 @@ function draw() {
     }
     if(gameState === 'play') {
 
-        if (frameCount % 60 === 30) {
+        if (frameCount % 300  === 30) {
             spawnFruit();
         }
         if(mouse.pressing()) {
