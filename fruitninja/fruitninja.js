fa9b5957c19a20e38010bfab9ed47649786975ae
ThemeGithub <olliken17@gmail.com>
2025-10-29 08:22:13 +0000

    Oct 29, 2025, 8:22 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -61,6 +61,7 @@ function draw() {
         rect(0,0,width,height);
         fill(255)
         textSize(48)
+        text('GAME OVER')
         textAlign(CENTER, CENTER)
         text("Score: " + score, width/2, height/2)
         text("Missed Fruits: " + missedFruits, width/2, height/2+40)
