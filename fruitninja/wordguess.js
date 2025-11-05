e693959d0b91ad70ef93fbef9e01b4afb91c0adc
ThemeGithub <olliken17@gmail.com>
2025-11-05 08:20:06 +0000

    Nov 5, 2025, 8:20 AM

--- fruitninja/wordguess.js
+++ fruitninja/wordguess.js
@@ -9,7 +9,7 @@ function setup() {
     new Canvas(600, 400);
 
     textBox = createInput();
-    textBox.position(width/2+550-textBox.width/2, 300);
+    textBox.position(width/2+350-textBox.width/2, 300);
     const word = words[Math.floor(Math.random() * words.length)];
     button = createButton('Guess');
     button.position(textBox.x+textBox.width+30,300)
